eeba140970f2b62de4832b04daa3ee28a318eb50
melihyelman <myelman17@gmail.com>
2024-09-09 14:26:09 +0300

    wiring beans

--- learning-spring-framework/src/main/java/com/melihyelman/learning_spring_framework/game/HelloWorldConfiguration.java
+++ learning-spring-framework/src/main/java/com/melihyelman/learning_spring_framework/game/HelloWorldConfiguration.java
@@ -3,10 +3,37 @@ package com.melihyelman.learning_spring_framework.game;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {};
+record Address(String firstLine, String city) {};
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name () {
         return "Hello World";
+    }
+
+    @Bean
+    public int age() {
+        return 23;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Melihyelman", 23,new Address("avcilar", "istanbul"));
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("kadikoy", "istanbul");
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(),age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name,age, address2);
     }
 }
