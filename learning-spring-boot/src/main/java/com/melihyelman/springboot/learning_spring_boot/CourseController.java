60afd138fb94afaf00ad214a6767d2668a37ff4a
melihyelman <myelman17@gmail.com>
2024-09-10 16:15:06 +0300

    api example

---
+++ learning-spring-boot/src/main/java/com/melihyelman/springboot/learning_spring_boot/CourseController.java
@@ -0,0 +1,21 @@
+package com.melihyelman.springboot.learning_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    // /courses
+    // Course: id,name,author
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1,"Learn AWS","melihyelman"),
+                new Course(2,"Learn Spring","melihyelman")
+        );
+    }
+}
