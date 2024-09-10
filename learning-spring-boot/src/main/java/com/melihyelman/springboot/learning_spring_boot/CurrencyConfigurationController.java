5e9d395abaf106dc62e0ad9360e12d53da80182f
melihyelman <myelman17@gmail.com>
2024-09-10 18:24:06 +0300

    actuator and configuration controller

---
+++ learning-spring-boot/src/main/java/com/melihyelman/springboot/learning_spring_boot/CurrencyConfigurationController.java
@@ -0,0 +1,23 @@
+package com.melihyelman.springboot.learning_spring_boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.List;
+
+@RestController
+public class CurrencyConfigurationController {
+    // /courses
+    // Course: id,name,author
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveAllCourses() {
+        return configuration;
+
+    }
+}
