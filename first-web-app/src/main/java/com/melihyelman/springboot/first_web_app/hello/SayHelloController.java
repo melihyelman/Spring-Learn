6eda64ff341788eece9ff7679bbc272fa01bc0c0
melihyelman <myelman17@gmail.com>
2024-09-10 23:16:37 +0300

    first step

---
+++ first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/hello/SayHelloController.java
@@ -0,0 +1,30 @@
+package com.melihyelman.springboot.first_web_app.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "say-hello";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>Hello World</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1>Hello World</h1>");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+}
