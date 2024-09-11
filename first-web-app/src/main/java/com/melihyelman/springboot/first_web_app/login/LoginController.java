6dcae91118babff9f9045288bf8bbc43f25c8b3f
melihyelman <myelman17@gmail.com>
2024-09-11 14:33:55 +0300

    logger

--- first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/login/LoginController.java
+++ first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.melihyelman.springboot.first_web_app.login;
 
+//import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +9,12 @@ import org.springframework.ui.ModelMap;
 
 @Controller
 public class LoginController {
-
+//    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String goToLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name",name);
-        System.out.println(name);
+//        logger.debug("Request param is {}",name);
+//        System.out.println(name);
         return "login";
     }
 }
