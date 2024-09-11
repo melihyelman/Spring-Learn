e81a002363502d6f3dc6684caebc705f161bb941
melihyelman <myelman17@gmail.com>
2024-09-11 19:55:51 +0300

    add spring security

---
+++ first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/security/SpringSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.melihyelman.springboot.first_web_app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+        UserDetails user = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username("melih")
+                .password("123123")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
