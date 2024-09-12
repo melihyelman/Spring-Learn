cec1d07a14a4f35df5166be3851e3ab7c2229011
melihyelman <myelman17@gmail.com>
2024-09-12 15:02:44 +0300

    configure spring security

--- first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/security/SpringSecurityConfiguration.java
+++ first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.melihyelman.springboot.first_web_app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.function.Function;
 
@@ -14,10 +18,10 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        UserDetails user1 = createNewUser( "melih","123123");
-        UserDetails user2 = createNewUser( "ahmet","321321");
+        UserDetails user1 = createNewUser("melih", "123123");
+        UserDetails user2 = createNewUser("ahmet", "321321");
 
-        return new InMemoryUserDetailsManager(user1,user2);
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
     private UserDetails createNewUser(String username, String password) {
@@ -36,5 +40,15 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .formLogin((form) -> form.permitAll())
+            .csrf((csrf) -> csrf.disable())
+            .headers(headers -> headers.frameOptions((frameOptions) -> frameOptions.disable()));
+
+        return http.build();
     }
 }
