ab386e602093913aebe049ef428db654d1de8657
melihyelman <myelman17@gmail.com>
2024-09-12 09:46:59 +0300

    setting up to newuser

--- first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/security/SpringSecurityConfiguration.java
+++ first-web-app/src/main/java/com/melihyelman/springboot/first_web_app/security/SpringSecurityConfiguration.java
@@ -14,16 +14,23 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails user1 = createNewUser( "melih","123123");
+        UserDetails user2 = createNewUser( "ahmet","321321");
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder
                 = input -> passwordEncoder().encode(input);
+
         UserDetails user = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("melih")
-                .password("123123")
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(user);
+        return user;
     }
 
     @Bean
